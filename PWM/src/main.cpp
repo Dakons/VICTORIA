6b7d8f3cd0d263f5d66579a6fdc24fe5a8d4802a
Dakons <76495215+Dakons@users.noreply.github.com>
2023-03-13 01:03:12 +0300

    kek
    ekek

--- PWM/src/main.cpp
+++ PWM/src/main.cpp
@@ -20,11 +20,25 @@ void loop()
  {
   
   // читаем новые значения частоты и заполнения с монитора порта
-  if (Serial.available() > 0) {
+  if (Serial.available() > 0) 
+  {
     freq = Serial.parseInt();
     dutyCycle = Serial.parseInt();
     ledcWriteTone(LEDC_TIMER, freq);
     ledcWrite(LEDC_CHANNEL, dutyCycle * 255 / 100);
   }
   delay(100);
+
+
+  float period = 1.0 / freq; // вычисляем длительность периода ШИМ
+  float pulse_width = (dutyCycle / 100.0) * period; // вычисляем длительность импульса ШИМ
+  float space_width = period - pulse_width; // вычисляем длительность пробела ШИМ
+
+  Serial.print("Pulse width: ");
+  Serial.print(pulse_width);
+  Serial.print(" us ");
+  Serial.print("Space width: ");
+  Serial.print(space_width);
+  Serial.println(" us");
+  delay(1000);
 }
