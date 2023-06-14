b3c5b54d4c17e30a048cd8ab4c52283e01a8b3c2
Dakons <76495215+Dakons@users.noreply.github.com>
2023-06-15 02:53:23 +0300

    Тильт такой же

--- PID_TEST_2_US_TILT/src/main.cpp
+++ PID_TEST_2_US_TILT/src/main.cpp
@@ -4,117 +4,143 @@
 
 #define IN1 2
 #define IN2 15
+#define ENA 5
 
 int onTime, offTime, period, total_direction;
+
+
 
 Adafruit_INA219 ina219_1;
 
 unsigned long sensTimer = 0;
 
+
+
+
 float dist_3[3] = {0.0, 0.0, 0.0};
 
-void configurePWM(int frequency, int dutyCycle);
+
+void configurePWM(int dutyCycle);
 void generatePWM(int pin);
 
 void serialPWM();
 
-void Motor_Drive(int direction);
 
-float HIGH_VAL, LOW_VAL, Err_T, prevErr_T, P_T, I_T, D_T, PID_T; // Для пида
+float HIGH_VAL, LOW_VAL, Err_T, prevErr_T, P_T, I_T, D_T, PID_T; //Для пида
 float Curr_1, Curr_2, Curr_3;
 float middle, dist, dist_filtered; // Для фильтрации
 
-float k;       // Коэффициент для бегущего среднего
-byte i, delta; // счётчики
+float k; //Коэффициент для бегущего среднего
+byte i, delta; //счётчики
 
-// Adafruit_INA219 ina219_2;
+
+//Adafruit_INA219 ina219_2;
 float middle_of_3(float a, float b, float c);
 float ReadAndFilterUS();
 
 float convertToMillimeters(float sensorValue);
 
-void PID_TILT(float VAL_LEFT, float Kp, float Ki, float Kd, float HIGH_VAL, float LOW_VAL);
+void PID_TILT(float VAL_LEFT, float VAL_RIGHT, float Kp, float Ki, float Kd, float HIGH_VAL, float LOW_VAL);
 
-void setup()
+void setup() 
 {
 
-  /*
-  Wire.begin(21, 22);
+/* 
+Wire.begin(21, 22);
 
-  //Serial.println("clean, median");
-  //ina219_1.setCalibration_32V_2A();
-  ina219_1.setCalibration_16V_400mA();
-  if (! ina219_1.begin()) {
-      //Serial.println("Failed to find INA219 chip");
-      while (1) { delay(10); }
-    }
-  //ina219_2.begin();
-  */
-  Serial.begin(115200);
+//Serial.println("clean, median");  
+//ina219_1.setCalibration_32V_2A();
+ina219_1.setCalibration_16V_400mA();
+if (! ina219_1.begin()) {       
+    //Serial.println("Failed to find INA219 chip");  
+    while (1) { delay(10); }
+  }
+//ina219_2.begin();
+*/
+Serial.begin(115200);
+pinMode(ENA, OUTPUT);
 
-  pinMode(IN1, OUTPUT);
-  pinMode(IN2, OUTPUT);
-  Motor_Drive(3);
+pinMode(IN1, OUTPUT);
+pinMode(IN2, OUTPUT);
 
-  // configurePWM(4, 90);
 
-  ledcSetup(0, 10, 8);
-  ledcAttachPin(IN1, 0); // Привязка пина к каналу ШИМ
+ledcSetup(0, 10, 8);
+ledcAttachPin(ENA, 0);  // Привязка пина к каналу ШИМ
+ledcWrite(0, 0);
 
-  ledcWrite(0, 0);
-  // ledcDetachPin(IN1);
+digitalWrite(IN2, HIGH);
+digitalWrite(IN1, HIGH);
 
-  P_T = 0.0;
-  I_T = 0.0;
-  D_T = 0.0;
-  PID_T = 0.0;
+P_T= 0.0;
+I_T = 0.0;
+D_T = 0.0;
+PID_T = 0.0;
+
 }
 
-void loop()
+
+
+void loop() 
 
 {
 
-  /*
-  CRIT_CHECK();
-  PID_TILT();
-  PID_TILT();
-  */
+/*
+CRIT_CHECK();
+PID_TILT();
+PID_TILT();
+*/
+PID_TILT(1200, 400, 1, 0, 0, 100, -100);
+Serial.println("---");
+delay(5000);
+
+PID_TILT(1000, 2346, 1, 0, 0, 100, -100);
+Serial.println("---");
+delay(5000);
+
+PID_TILT(100, 120, 1, 0, 0, 100, -100);
+Serial.println("---");
+delay(5000);
+
+PID_TILT(-2000, -1000, 1, 0, 0, 100, -100);
+Serial.println("---");
+delay(5000);
 }
 
 float ReadAndFilterUS()
 {
   if (millis() - sensTimer > 1)
-  { // измерение и вывод каждые 50 мс
+  {                          // измерение и вывод каждые 50 мс
     dist = ina219_1.getCurrent_mA();
-
-    delta = abs(dist_filtered - dist); // расчёт изменения с предыдущим
-    if (delta > 1)                     // если большое - резкий коэффициент
+    
+    delta = abs(dist_filtered - dist);                      // расчёт изменения с предыдущим
+    if (delta > 1)  // если большое - резкий коэффициент
     {
       k = 0.95;
-    }
+    } 
     else if (delta <= 1 && delta > 0.3)
     {
       k = 0.8;
-    }
-    else if (delta <= 0.3 && delta >= 0.1)
+    }     
+    else if (delta <= 0.3 && delta >=0.1)
     {
       k = 0.1;
     }
-    else if (delta <= 0.1)
+      else if (delta <= 0.1)
     {
       k = 0.05;
     }
-    // если маленькое - плавный коэффициент
+                                   // если маленькое - плавный коэффициент
 
-    dist_filtered += (dist - dist_filtered) * k; // фильтр "бегущее среднее"
+    dist_filtered += ( dist -dist_filtered) * k;     // фильтр "бегущее среднее"
+    
+    sensTimer = millis();                                   // сбросить таймер
 
-    sensTimer = millis(); // сбросить таймер
   }
   return dist_filtered + 0.35; //+0.43
 }
 
 float convertToMillimeters(float sensorValue)
-{
+ {
   if (sensorValue < 3.7)
   {
     return 0.0;
@@ -123,7 +149,7 @@ float convertToMillimeters(float sensorValue)
   {
     sensorValue = 4.0;
   }
-
+  
   if (sensorValue > 22.0)
   {
     return 1.0;
@@ -133,10 +159,11 @@ float convertToMillimeters(float sensorValue)
     sensorValue = 20.0;
   }
 
-  float minSensorValue = 4.0;    // Минимальное значение с токового датчика (в мА)
+
+  float minSensorValue = 4.0;  // Минимальное значение с токового датчика (в мА)
   float minMillimeters = 100.0;  // Минимальное значение в миллиметрах
-  float maxSensorValue = 20.0;   // Максимальное значение с токового датчика (в мА)
-  float maxMillimeters = 2000.0; // Максимальное значение в миллиметрах
+  float maxSensorValue = 20.0;  // Максимальное значение с токового датчика (в мА)
+  float maxMillimeters = 2000.0;  // Максимальное значение в миллиметрах
 
   // Выполняем линейную интерполяцию
   float millimeters = ((sensorValue - minSensorValue) / (maxSensorValue - minSensorValue)) * (maxMillimeters - minMillimeters) + minMillimeters;
@@ -144,113 +171,110 @@ float convertToMillimeters(float sensorValue)
   return millimeters;
 }
 
+
 void PID_TILT(float VAL_LEFT, float VAL_RIGHT, float Kp, float Ki, float Kd, float HIGH_VAL, float LOW_VAL)
 
 {
-  Err_T = VAL_LEFT - VAL_RIGHT;
 
-  if (Err_T > HIGH_VAL)
-  {
-    Err_T = Err_T - HIGH_VAL;
-  }
-  else if (Err_T < LOW_VAL)
-  {
-    Err_T = Err_T - LOW_VAL;
-  }
-  else
-  {
-    Err_T = 0;
-  }
+Err_T = VAL_LEFT - VAL_RIGHT;
 
-  P_T = Err_T * Kp;
-  I_T = I_T + Err_T * Ki;
-  D_T = (Err_T - prevErr_T) * Kd;
-
-  if (I_T > 2000.0 || I_T < -2000.0)
-  {
-    I_T = 0;
-  }
-
-  PID_T = P_T + I_T + D_T;
-
-  Serial.print(" 1 PID_T - ");
-  Serial.println(PID_T);
-
-  if (PID_T < 0)
-  {
-    Motor_Drive(2);
-    PID_T = map(PID_T, 0, -2500, 0, 100);
-
-    PID_T = constrain(PID_T, 0, 100);
-  }
-  else if (PID_T > 0)
-  {
-    Motor_Drive(1);
-    PID_T = map(PID_T, 0, 2500, 0, 100);
-
-    PID_T = constrain(PID_T, 0, 100);
-  }
-  else
-  {
-    Motor_Drive(3);
-    // Serial.println(PID);
-  }
-
-  // PID = map(PID, -1000, 2000, -100, 100);
-  // PID = map(PID, -100, 100, 0, 100);
-
-  Serial.print(" 2 PID_T - ");
-  Serial.println(PID_T);
-
-  configurePWM(10, PID_T);
-}
-
-void configurePWM(int frequency, int dutyCycle)
+if (Err_T > HIGH_VAL)
 {
-  ledcDetachPin(IN1);
-  ledcAttachPin(IN1, 0); // Привязка пина к каналу ШИМ
-  ledcSetup(0, frequency, 8);
-  Serial.print("DC -");
-  Serial.println(dutyCycle);
-  Serial.print("dir -");
-  Serial.println(total_direction);
-
-  switch (total_direction)
-  {
-  case 1:
-    ledcWrite(0, map(dutyCycle, 0, 100, 0, 255));
-    break;
-
-  case 2:
-    ledcWrite(0, map(dutyCycle, 0, 100, 255, 0));
-    break;
-  case 3:
-    // ledcDetachPin(IN1);
-    break;
-  }
+  Err_T = Err_T - HIGH_VAL;
+  
 }
-
-void Motor_Drive(int direction)
+else if (Err_T < LOW_VAL)
 {
-  total_direction = direction;
-  // Serial.println(direction);
-  switch (direction)
-  {
-  case 1: // Если вперёд
-    digitalWrite(IN2, LOW);
-    // digitalWrite(IN1, HIGH);
-    // Настройка pwm
-    break;
-
-  case 2: // Если назад
-    digitalWrite(IN2, HIGH);
-    // digitalWrite(IN1, LOW);
-    // Настройка pwm
-    break;
-
-  case 3: // Тормоз
-    ledcDetachPin(IN1);
-    digitalWrite(IN1, HIGH);
-    digitalWrite(IN2, HIGH);
-  }
+  Err_T = Err_T - LOW_VAL;
 }
+else
+{
+Err_T = 0;
+} 
+
+P_T = Err_T * Kp;
+I_T = I_T + Err_T * Ki;
+D_T = (Err_T - prevErr_T) * Kd;
+
+if (I_T>2000.0 || I_T<-2000.0) 
+{
+  I_T = 0;
+}
+
+PID_T = P_T + I_T + D_T;
+
+Serial.print(" 1 PID_T - ");
+Serial.println(PID_T);
+
+if (PID_T < 0)
+{
+total_direction = 1;
+PID_T = map(PID_T, 0, -2500, 0, 100);
+
+PID_T = constrain(PID_T, 0, 100);
+}
+else if (PID_T > 0)
+{
+total_direction = 2;
+PID_T = map(PID_T, 0, 2500, 0, 100);
+
+PID_T = constrain(PID_T, 0, 100);
+
+}
+else
+{
+total_direction = 3;
+//Serial.println(PID);
+} 
+
+//PID = map(PID, -1000, 2000, -100, 100);
+//PID = map(PID, -100, 100, 0, 100);
+
+Serial.print(" 2 PID_T - ");
+Serial.println(PID_T);
+
+configurePWM(PID_T);
+
+}
+
+
+void configurePWM(int dutyCycle)
+{
+
+switch (total_direction)
+{
+case 1:
+
+//Motor_Drive(1);
+
+
+digitalWrite(IN2, LOW);
+digitalWrite(IN1, HIGH);
+ledcWrite(0, map(dutyCycle, 0, 100, 0, 255));
+
+break;
+
+case 2:
+//Motor_Drive(2);
+digitalWrite(IN2, HIGH);
+digitalWrite(IN1, LOW);
+ledcWrite(0, map(dutyCycle, 0, 100, 0, 255));//реверснул
+
+
+  break;
+case 3:
+
+digitalWrite(IN2, HIGH);
+digitalWrite(IN1, HIGH);
+ledcWrite(0, 0);
+
+break;
+}
+
+Serial.print("DC -");
+Serial.println(dutyCycle);
+Serial.print("dir -");
+Serial.println(total_direction);
+
+}
+
